ed54c7a34873a5d07d2341b8aa4a28653307c073
omesh8489 <135409960+omesh8489@users.noreply.github.com>
2024-09-28 03:22:39 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,125 @@
+/* .App {
+  text-align: center;
+} */
+
+/* .App, .App-header, .App-footer{
+  background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmcu0aW7tyi3BgrZfQw2yc7HDE9yy05o1vr2NfoAxXC9g7l85IoBx430KRgC2z_D2Bt2g&usqp=CAU");
+} */
+body{
+  background-image: url("ppo.jpg") ;
+  
+
+}
+.App{
+  color:white;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: black;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  /* color: white; */
+}
+
+.App-link {
+  /* color: #61dafb; */
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.hj{
+  margin:90px;
+
+}
+.kl{
+  margin-left:20px ;
+  background-color:black;
+  /* opacity:0.3; */
+  /* color:black; */
+  font-size:20px;
+}
+.gh{
+  position:absolute;
+  /* margin-bottom:444px; */
+  /* left:10px; */
+  /* font-size:2 px; */
+ margin-right:10px;
+  margin-left:720px;
+  margin-top:50px;
+  /* display: flex;
+  flex-direction: column; */
+  /* align-items: center;
+  justify-content: center; */
+  /* margin-right:30px; */
+  /* margin-bottom:100 px; */
+  /* margin: auto;
+  width: 50%;
+  border: 3px solid green;
+  padding: 10px; */
+}
+.ff{
+  margin-left:610px;
+  margin-top:400px;
+}
+textarea
+{
+
+  background-color:black;
+  opacity:0.3;
+  color:white;
+}
+.kk
+{
+  color: #90ee90;
+  font-size:20px;
+}
+.jj
+{
+  color: yellow;
+  font-size:20px;
+}
+.ll
+{
+  color: red;
+  font-size:20px;
+}
+.qw{
+  
+  margin-right:200px;
+}
+.po{
+  position:absolute;
+  margin-bottom:0px;
+  /* margin-bottom:444px; */
+  /* left:10px; */
+  /* font-size:2 px; */
+ 
+  /* margin-left:20px; */
+  /* margin-right:30px; */
+  /* margin-bottom:100 px; */
+  /* margin: auto;
+  width: 50%;
+  border: 3px solid green;
+  padding: 10px; */
+}
