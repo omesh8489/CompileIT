937a6cc1f958cbd05e9ed051a75dfe98595e3941
omesh8489 <135409960+omesh8489@users.noreply.github.com>
2024-09-28 03:25:16 +0530

    Create defaultStubs.js

---
+++ frontend/src/defaultStubs.js
@@ -0,0 +1,15 @@
+const stubs = {};
+
+stubs.cpp = `#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+  cout<<"Hello world!\\n";
+  
+  return 0;
+}
+`;
+
+stubs.py = `print("Hello world!")`;
+
+export default stubs;
