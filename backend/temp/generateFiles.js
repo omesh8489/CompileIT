36129cb78c133fdffdd0d11cae6ebb4c85c0b3b2
omesh8489 <135409960+omesh8489@users.noreply.github.com>
2024-09-28 03:19:07 +0530

    Create generateFiles.js

---
+++ backend/temp/generateFiles.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+const dirCodes = path.join(__dirname, "codes");
+
+if (!fs.existsSync(dirCodes)) {
+  fs.mkdirSync(dirCodes, { recursive: true });
+}
+
+const generateFile = async (format, content) => {
+  const jobId = uuid();
+  const filename = `${jobId}.${format}`;
+  const filepath = path.join(dirCodes, filename);
+  await fs.writeFileSync(filepath, content);
+  return filepath;
+};
+const ac=async(input)=>{
+  return input;
+}
+const bc=async(input)=>{
+  return code;
+}
+
+module.exports = {
+  generateFile,ac,bc
+};
